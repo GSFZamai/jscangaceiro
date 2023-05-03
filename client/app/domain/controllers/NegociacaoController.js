fdfdec893f7e496c0f1e011cb72d2ccd83a27fc3
Gabriel Zamai <gabriel.zamai@bkbank.com.br>
2023-05-03 08:33:05 -0300

    criado classe Bind, concluido cap 10

--- client/app/domain/controllers/NegociacaoController.js
+++ client/app/domain/controllers/NegociacaoController.js
@@ -5,37 +5,16 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
-        const self = this;
-        this._negociacoes = new Proxy(new Negociacoes(), {
-            get(target, prop) {
-                if(typeof(target[prop]) === typeof(Function) && ['adiciona', 'esvazia'].includes(prop)) {
+        this._negociacoes = new Bind(new Negociacoes(), new NegociacoesView("#negociacoes"), "adiciona", "esvazia")
 
-                    return function() {
-                        console.log(`"${prop}" disparou na armadilha`);
-                        target[prop].apply(target, arguments);
-                        self._negociacoesView.update(target);
-                    }
+        this._mensagem = new Bind(new Mensagem(), new MensagemView("#mensagemView"), "texto");
 
-                }else {
-                    return target[prop];
-                }
-            }
-        });
-
-        this._negociacoesView = new NegociacoesView("#negociacoes");
-
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView("#mensagemView");
-        this._mensagemView.update(this._mensagem);
-
-        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event) {
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = "Negociação adicionada com sucesso";
-        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
 
